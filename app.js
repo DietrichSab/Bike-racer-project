75f354fb9e133db807fd78693efc70d52cd62c8c
DietrichSab <140287531+DietrichSab@users.noreply.github.com>
2023-07-22 20:24:17 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+﻿// Sample array to store racers and their times
+let racers = [];
+
+function addRacer() {
+    const nameInput = document.getElementById('nameInput');
+    const timeInput = document.getElementById('timeInput');
+
+    const name = nameInput.value.trim();
+    const time = parseInt(timeInput.value);
+
+    if (name === '' || isNaN(time)) {
+        alert('Please enter valid name and time.');
+        return;
+    }
+
+    const racer = { name, time };
+    racers.push(racer);
+
+    updateRacersList();
+    nameInput.value = '';
+    timeInput.value = '';
+}
+
+function updateRacersList() {
+    const racersList = document.getElementById('racersList');
+    racersList.innerHTML = '';
+
+    racers.forEach((racer) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${racer.name}: ${racer.time} seconds`;
+        racersList.appendChild(listItem);
+    });
+}
+
+
